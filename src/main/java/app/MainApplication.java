45b9512971d73ac32e5ce606130ecc2ce86c57bc
aBgAmeuR <a.josset@yahoo.com>
2023-04-04 15:56:43 +0100

    commit

--- src/main/java/app/MainApplication.java
+++ src/main/java/app/MainApplication.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/view/Game.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 800);
         scene.getStylesheets().add(MainApplication.class.getResource("/css/Style.css").toExternalForm());
