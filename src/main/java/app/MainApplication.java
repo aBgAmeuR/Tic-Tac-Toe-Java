4a127c6c732166ab8f9ad3a1a8384e7f7fd52f94
aBgAmeuR <a.josset@yahoo.com>
2023-04-15 11:11:54 +0100

    commit

--- src/main/java/app/MainApplication.java
+++ src/main/java/app/MainApplication.java
@@ -16,8 +16,8 @@ public class MainApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/view/Game.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 800);
         scene.getStylesheets().add(MainApplication.class.getResource("/css/Style.css").toExternalForm());
-        
         stage.getIcons().add(new Image(MainApplication.class.getResource("/images/icon.png").openStream()));
+        
         stage.setTitle("Tic-Tac-Toe");
         stage.setScene(scene);
         stage.show();
