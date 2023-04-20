cb59020b061f69e7a5e1ee84c88569d9dfd64c5e
aBgAmeuR <a.josset@yahoo.com>
2023-04-20 12:22:14 +0100

    commit

--- src/main/java/app/MainApplication.java
+++ src/main/java/app/MainApplication.java
@@ -15,12 +15,13 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/view/Game.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 800);
-        
         scene.getStylesheets().add(MainApplication.class.getResource("/css/Style.css").toExternalForm());
+
         stage.getIcons().add(new Image(MainApplication.class.getResource("/images/icon.png").openStream()));
         stage.setTitle("Tic-Tac-Toe");
         stage.setScene(scene);
-        stage.show();
+
+        // stage.show();
     }
 
 
